4c553b8dc19ed67f32c73663832a9346c64a26d8
bubbykinz <sigalhayden@gmail.com>
2020-09-07 23:56:07 -0700

    Digging part 1
    Added code for freezing position and unfreezing position of sand. Also started putting in code for digging

--- DigProject/Assets/Scripts/PlayerController.cs
+++ DigProject/Assets/Scripts/PlayerController.cs
@@ -2,17 +2,21 @@
 using System.Collections.Generic;
 using System.IO;
 using UnityEngine;
+using UnityEngine.Rendering;
 
 public class PlayerController : MonoBehaviour
 {
     public Rigidbody2D rigidBody;
+    public Collider2D shovel;
+    public float colW;
+    public float colH;
 
     public float moveSpeed = 5f;
     public float jumpSpeed = 5f;
     // Start is called before the first frame update
     void Start()
     {
-        
+
     }
 
     // Update is called once per frame
@@ -21,10 +25,22 @@ public class PlayerController : MonoBehaviour
         Vector3 movement = new Vector3(Input.GetAxis("Horizontal"), 0f, 0f);
         transform.position += movement * Time.deltaTime * moveSpeed;
 
-        if (Input.GetKeyUp(KeyCode.Space))
+        if (Input.GetKeyDown(KeyCode.Space))
         {
             if (this.rigidBody != null)
                 this.rigidBody.velocity += new Vector2(0f, jumpSpeed);
+        }
+        if (Input.GetKeyDown(KeyCode.E))
+        {
+            if (this.shovel != null)
+            {
+                Collider[] array = Physics.OverlapBox(this.shovel.transform.localPosition, new Vector3(colW, colH, 0f));
+                foreach ( Collider in array) ;
+            }
+
+
+
+            object GetComponent(SandPhysics.GameOb
         }
     }
 }
---
+++ DigProject/Assets/Scripts/SandPhysics.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SandPhysics : MonoBehaviour
+{
+    public bool appliedVelocity = false;
+    public Rigidbody2D rigidBody2D;
+    GameObject.Find(Shovel);
+   
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        rigidBody2D = gameObject.GetComponent<Rigidbody2D>();
+    }
+
+    public void ApplyVelocity()
+    {
+        rigidBody2D.constraints = RigidbodyConstraints2D.None;
+        this.rigidBody2D.velocity = new Vector2 (3, 3);
+        this.appliedVelocity = true;
+    }
+    public void ApplyDefault()
+    {
+        rigidBody2D.constraints = RigidbodyConstraints2D.FreezePositionX | RigidbodyConstraints2D.FreezePositionY;
+        this.rigidBody2D.velocity = Vector2.zero;
+        this.appliedVelocity = false;
+
+        // Update is called once per frame
+        void Update()
+    {
+        if (rigidBody2D.velocity = Vector2 (0,0))
+            {
+
+            }
+    }
+    void OnCollisionEnter(Collision collision)
+    {
+            
+            ApplyVelocity;
+    }
+}
